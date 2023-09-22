0903e232126c3713c740e10e43a6b4de58343315
hrvadl <youngwwad@gmail.com>
2023-09-22 11:26:35 +0300

    feat(frontend): init structure

---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var partials = []string{
+	"./cmd/web/templates/base.layout.gohtml",
+	"./cmd/web/templates/header.partial.gohtml",
+	"./cmd/web/templates/footer.partial.gohtml",
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+
+	log.Println("Starting front end service on port 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Panic(err)
+	}
+}
+
+func render(w http.ResponseWriter, t string) {
+	templateSlice := append(
+		make([]string, 0, len(partials)+1),
+		append([]string{fmt.Sprintf("./cmd/web/templates/%s", t)}, partials...)...,
+	)
+
+	tmpl, err := template.ParseFiles(templateSlice...)
+
+	if err != nil {
+		log.Printf("error parsing template: %v", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		log.Printf("error executing template: %v", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
